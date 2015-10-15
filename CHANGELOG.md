6ba6d86cf8c44b5d426bddbf378bf8dd8ad85fc5
Adam Leff <aleff@chef.io>
2015-10-15 09:37:15 -0700

    version bump to 1.0.0 and add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# oraclecloud client gem CHANGELOG
+
+## v1.0.0 (2015-10-15)
+* Initial release
+
--- lib/oraclecloud/version.rb
+++ lib/oraclecloud/version.rb
@@ -16,5 +16,5 @@
 # limitations under the License.
 #
 module OracleCloud
-  VERSION = '1.0.0.rc.1'
+  VERSION = '1.0.0'
 end
